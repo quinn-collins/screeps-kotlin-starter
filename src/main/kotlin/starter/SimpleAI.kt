dc26992c9232d2ab3f85be3ded14c64f8c546f41
Quinn Collins <collinsqui@gmail.com>
2019-05-14 07:46:39 -0400

    broke mission assign logic

--- src/main/kotlin/starter/SimpleAI.kt
+++ src/main/kotlin/starter/SimpleAI.kt
@@ -13,8 +13,8 @@ fun gameLoop() {
 
     houseKeeping(Game.creeps)
     spawnCreeps(Game.creeps.values, mainSpawn)
-    assignMission(Game.creeps.values)
-    runCreepWork(Game.creeps.values, mainSpawn.room)
+    assignMission(Game.creeps.values, mainSpawn)
+    runCreepWork(Game.creeps, mainSpawn)
 }
 
 private fun houseKeeping(creeps: Record<String, Creep>) {
@@ -74,7 +74,11 @@ private fun spawnCreeps(creeps: Array<Creep>, spawn: StructureSpawn) {
                 memory = jsObject<CreepMemory> { this.role = role; this.mission = Mission.UNASSIGNED }
             })
 
-            when (code) {
+            when (val code = spawn.spawnCreep(role.body, newName, options {
+                    memory = jsObject<CreepMemory> {
+                        this.role = role
+                        this.mission = Mission.UNASSIGNED }
+                })) {
                 OK -> console.log("spawning $newName with body ${role.body}")
                 ERR_BUSY, ERR_NOT_ENOUGH_ENERGY -> run { } // do nothing
                 else -> console.log("unhandled error code $code")
@@ -82,11 +86,10 @@ private fun spawnCreeps(creeps: Array<Creep>, spawn: StructureSpawn) {
             return
         }
 
-
     }
 }
 
-enum class Mission() {
+enum class Mission {
     HARVEST,
     SPAWNER,
     CONTROLLER,
@@ -95,121 +98,48 @@ enum class Mission() {
     HEAL,
     UNASSIGNED
 }
-fun assignMission(creeps: Array<Creep>) {
+fun assignMission(creeps: Array<Creep>, spawn: StructureSpawn) {
     for(creep in creeps) {
-        if(creep.carry.energy == 0) {
-            creep.memory.mission = Mission.HARVEST
+        if(creep.memory.mission == Mission.UNASSIGNED) {
+            if(spawn.energy < spawn.energyCapacity) {
+                creep.memory.mission = Mission.SPAWNER
+            } else {
+                creep.memory.mission = Mission.CONTROLLER
+            }
+        } else {
+            if( creep.carry.energy == 0) {
+                creep.memory.mission = Mission.HARVEST
+            } else if ( creep.carry.energy == creep.carryCapacity) {
+                creep.memory.mission = Mission.UNASSIGNED
+            }
         }
     }
 }
 
-fun runCreepWork(creeps: Array<Creep>, room: Room) {
-    val sources = room.find(FIND_SOURCES)
-   for(creep in creeps) {
-        val sources = room.find(FIND_SOURCES)
-        if (sources[0] == ERR_NOT_IN_RANGE) {
-            moveTo(sources[0].pos)
-        }
+fun runCreepWork(creeps: Record<String, Creep>, spawn: StructureSpawn) {
+   for((_, creep) in creeps) {
+       if(creep.memory.mission == Mission.HARVEST) {
+           val sources = spawn.room.find(FIND_SOURCES)
+           if (creep.moveTo(sources[0]) == ERR_NOT_IN_RANGE) {
+               creep.moveTo(sources[0].pos)
+           } else {
+               creep.harvest(sources[0])
+           }
+
+       } else if (creep.memory.mission == Mission.SPAWNER){
+          if(creep.moveTo(spawn.pos) == ERR_NOT_IN_RANGE) {
+             creep.moveTo(spawn.pos)
+          } else {
+              creep.transfer(spawn, RESOURCE_ENERGY)
+          }
+       } else {
+           if(creep.moveTo(spawn.room.controller!!) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(spawn.room.controller!!)
+           } else {
+               creep.transfer(spawn.room.controller!!, RESOURCE_ENERGY)
+           }
+       }
    }
 }
-
-//    val mainSpawn: StructureSpawn = Game.spawns.values.firstOrNull() ?: return
-//
-//    //delete memories of creeps that have passed away
-//    houseKeeping(Game.creeps)
-//
-//    //make sure we have at least some creeps
-//    spawnCreeps(Game.creeps.values, mainSpawn)
-//
-//    // build a few extensions so we can have 550 energy
-//    val controller = mainSpawn.room.controller
-//    if (controller != null && controller.level >= 2) {
-//        when (controller.room.find(FIND_MY_STRUCTURES).count { it.structureType == STRUCTURE_EXTENSION }) {
-//            0 -> controller.room.createConstructionSite(30, 17, STRUCTURE_EXTENSION)
-//            1 -> controller.room.createConstructionSite(31, 17, STRUCTURE_EXTENSION)
-//            2 -> controller.room.createConstructionSite(32, 17, STRUCTURE_EXTENSION)
-//            3 -> controller.room.createConstructionSite(33, 17, STRUCTURE_EXTENSION)
-//            4 -> controller.room.createConstructionSite(34, 17, STRUCTURE_EXTENSION)
-//            5 -> controller.room.createConstructionSite(35, 17, STRUCTURE_EXTENSION)
-//            6 -> controller.room.createConstructionSite(36, 17, STRUCTURE_EXTENSION)
-//        }
-//    } else if (controller != null && controller.level >= 3) {
-//        when(controller.room.find(FIND_MY_STRUCTURES).count { it.structureType == STRUCTURE_TOWER}) {
-//            0 -> controller.room.createConstructionSite(35, 24, STRUCTURE_TOWER)
-//        }
-//    }
-//
-//    //spawn a big creep if we have plenty of energy
-//    for ((_, room) in Game.rooms) {
-//        if (room.energyAvailable >= 550) {
-//            mainSpawn.spawnCreep(
-//                    arrayOf(
-//                            WORK,
-//                            WORK,
-//                            WORK,
-//                            WORK,
-//                            CARRY,
-//                            MOVE,
-//                            MOVE
-//                    ),
-//                    "HarvesterBig_${Game.time}",
-//                    options {
-//                        memory = jsObject<CreepMemory> {
-//                            this.role = Role.HARVESTER
-//                        }
-//                    }
-//            )
-//            console.log("hurray!")
-//        }
-//    }
-//
-//    for ((_, creep) in Game.creeps) {
-//        when (creep.memory.role) {
-//            Role.HARVESTER -> creep.harvest()
-//            Role.BUILDER -> creep.build()
-//            Role.UPGRADER -> creep.upgrade(mainSpawn.room.controller!!)
-//            else -> creep.pause()
-//        }
-//    }
-//
-//}
-//
-//private fun spawnCreeps(
-//        creeps: Array<Creep>,
-//        spawn: StructureSpawn
-//) {
-//
-//    val body = arrayOf<BodyPartConstant>(WORK, CARRY, MOVE)
-//
-//    if (spawn.room.energyAvailable < body.sumBy { BODYPART_COST[it]!! }) {
-//        return
-//    }
-//
-//    val role: Role = when {
-//        creeps.count { it.memory.role == Role.HARVESTER } < 2 -> Role.HARVESTER
-//
-//        creeps.none { it.memory.role == Role.UPGRADER } -> Role.UPGRADER
-//
-//        spawn.room.find(FIND_MY_CONSTRUCTION_SITES).isNotEmpty() &&
-//                creeps.count { it.memory.role == Role.BUILDER } < 2 -> Role.BUILDER
-//
-//        else -> return
-//    }
-//
-//    val newName = "${role.name}_${Game.time}"
-//    val code = spawn.spawnCreep(body, newName, options {
-//        memory = jsObject<CreepMemory> { this.role = role }
-//    })
-//
-//    when (code) {
-//        OK -> console.log("spawning $newName with body $body")
-//        ERR_BUSY, ERR_NOT_ENOUGH_ENERGY -> run { } // do nothing
-//        else -> console.log("unhandled error code $code")
-//    }
-//}
-//
-
-var CreepMemory.building: Boolean by memory { false }
-var CreepMemory.pause: Int by memory { 0 }
 var CreepMemory.role by memory(Role.UNASSIGNED)
 var CreepMemory.mission by memory(Mission.UNASSIGNED)
